02fc7ab4f6c2591026ff1d5fd40f1fb1a49c3e1d
IzzatAliev <alibax1515@gmail.com>
2022-02-17 21:27:19 +0200

    done

--- src/main/java/com/ua/Хэллоуин.java
+++ src/main/java/com/ua/Хэллоуин.java
@@ -1,12 +1,11 @@
 package com.ua;
 
-import java.util.stream.Stream;
+import java.util.Arrays;
 
 public class Хэллоуин {
 
-    public static Integer summ = 0;
-    private static final Integer[][] example1 = new Integer[][]{{1,3},{2,2},{3,1}};
-    private static final Integer[][] example2 = new Integer[][]{{5,10},{2,5},{4,7},{3,9}};
+    private static final Integer[][] example1 = new Integer[][]{{1, 3}, {2, 2}, {3, 1}};
+    private static final Integer[][] example2 = new Integer[][]{{5, 10}, {2, 5}, {4, 7}, {3, 9}};
     private static final Integer[][] example3 = new Integer[][]{
             {35, 14}, {57, 99}, {70, 48},
             {50, 70}, {59, 24}, {48, 72},
@@ -21,18 +20,36 @@ public class Хэллоуин {
             {84, 26}, {51, 52}, {21, 16}};
 
     public static void main(String[] args) {
-        output(example1);
-        output(example2);
-        output(example3);
+        Хэллоуин my = new Хэллоуин();
+        System.out.println("First Test");
+        my.output(example1, 4);
+        System.out.println("--------------------");
+        System.out.println("Second Test");
+        my.output(example2, 10);
+        System.out.println("--------------------");
+        System.out.println("Third Test");
+        my.output(example3, 270);
     }
 
-    public static void output(Integer[][] array) {
-        for (Integer[] anArray : array) {
-            summ += Stream
-                    .of(anArray)
-                    .mapToInt(s -> s)
-                    .reduce(1, (a, b) -> a * b);
+
+    private void output(Integer[][] array, Integer count) {
+        int sum = 0;
+        sort(array);
+        System.out.println("sweetnessMaxCount = " + count);
+        for (int i = 0; 0 < count; i++) {
+            if (count > array[i][0]) {
+                count -= array[i][0];
+                sum += array[i][0] * array[i][1];
+            } else {
+                sum += count * array[i][1];
+                break;
+            }
         }
-        System.out.println(summ);
+        System.out.println("output = " + sum);
+    }
+
+    private void sort(Integer[][] array) {
+        Arrays.sort(array, (a, b) -> Integer.compare(b[1],a[1]));
+        System.out.println("sweetnessTypes = " + Arrays.deepToString(array));
     }
 }
